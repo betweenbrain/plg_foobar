d60b442b5e6336e1d5f30930618b5a887acafd8f
Matt Thomas <matt@betweenbrain.com>
2014-03-31 20:56:24 -0400

    Basic system plugin

---
+++ foobar.php
@@ -0,0 +1,12 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       foobar.php
+ * Created    3/31/14 8:51 PM 
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+ 
+ 
